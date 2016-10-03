6081ab22e3fd425358cc0a899942b3f4eff371d7
TomboyFluttershy <jen.oakley.artist@gmail.com>
2016-10-03 15:45:05 -0600

    Set up a basic sound skeleton with a sample background track snippet

---
+++ Assets/Scripts/SoundManagement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManagement : MonoBehaviour {
+
+	public AudioSource sfxSource;
+	public AudioSource musicSource;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
