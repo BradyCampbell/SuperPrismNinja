e449ccdf6a69a4a4f33c3261d6c85dcd378b5b0f
TomboyFluttershy <jen.oakley.artist@gmail.com>
2016-10-11 16:16:27 -0600

    Massive Playable Prototype Update! Please read full description.
    Animations added, movement aligned and constrained, enemy spawns aligned
    and set for a not random, but highly variable pattern, enemies arranged
    and flipped for shadow side, camera set up, some scripts tweaked,
    players randomly rotating on dodge fixed (for real this time, there was
    a collision lock missing), background music extended to full and set for
    a proper loop, some extra organization added, etc. for other things I
    may have forgotten since there's a lot.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Movement : MonoBehaviour 
 {
+	private int posTrack;
 	// Use this for initialization
 	void Start () 
 	{
@@ -12,16 +13,18 @@ public class Movement : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Input.GetKeyDown(KeyCode.UpArrow))
+		if (Input.GetKeyDown(KeyCode.UpArrow) && (posTrack < 2))
 		{
 			Vector3 position = this.transform.position;
-			position.y++;
+			position.y += 1.32f;
+			posTrack++;
 			this.transform.position = position;
 		}
-		if (Input.GetKeyDown(KeyCode.DownArrow))
+		if (Input.GetKeyDown(KeyCode.DownArrow) && (posTrack > 0))
 		{
 			Vector3 position = this.transform.position;
-			position.y--;
+			position.y -= 1.32f;
+			posTrack--;
 			this.transform.position = position;
 		}
 	}
--- Assets/Scripts/ReflectMovement.cs
+++ Assets/Scripts/ReflectMovement.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class ReflectMovement : MonoBehaviour 
 {
-
+	private int posTrack;
 	// Use this for initialization
 	void Start ()
 	{
@@ -13,16 +13,18 @@ public class ReflectMovement : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Input.GetKeyDown(KeyCode.DownArrow))
+		if (Input.GetKeyDown(KeyCode.DownArrow) && (posTrack > 0))
 		{
 			Vector3 position = this.transform.position;
-			position.y++;
+			position.y += 1.32f;
+			posTrack--;
 			this.transform.position = position;
 		}
-		if (Input.GetKeyDown(KeyCode.UpArrow))
+		if (Input.GetKeyDown(KeyCode.UpArrow) && (posTrack < 2))
 		{
 			Vector3 position = this.transform.position;
-			position.y--;
+			position.y -= 1.32f;
+			posTrack++;
 			this.transform.position = position;
 		}
 	}
