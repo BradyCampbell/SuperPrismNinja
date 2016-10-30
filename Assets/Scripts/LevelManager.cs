9e3b78c83271ddc54d3bb634a4b9bae1c4d38827
Britt H <Kitreiki@gmail.com>
2016-10-30 14:32:14 -0600

    Now with menu and a scene file
    Hi guys, I made a basic start and end screen and hooked it up to a lvl
    manager so you can actually go to the game from the start menu or to the
    menu or game from the end scene. an engineer will need to hook up the
    dying to trigger end scene part. Also I made a folder to put the scenes
    into since there are now multiple.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel (string name){
+		Debug.Log("Level load requested for: " + name);
+        Application.LoadLevel(name);
+	}
+	
+	public void QuitRequest (){
+		Debug.Log("Run Away!!!!!");
+		Application.Quit();
+	}
+	
+	public void BackStart (string name){
+		Debug.Log("Return to Start");
+		Application.LoadLevel(name);
+	}
+	
+	
+}
