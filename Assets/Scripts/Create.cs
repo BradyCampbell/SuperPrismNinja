2327101b76d2b98ac596658bca990874d89d14a6
melody <changmelody2727@gmail.com>
2016-10-31 15:07:24 -0600

    no message

--- Assets/Scripts/Create.cs
+++ Assets/Scripts/Create.cs
@@ -4,17 +4,21 @@ using System.Collections;
 public class Create : MonoBehaviour {
 
 	public GameObject obj;
+	public GameObject obj1;
+	public GameObject obj2;
+	public GameObject obj3;
+	public GameObject obj4;
+	public GameObject obj5;
 	public Transform generationPoint;
 
 	private float xPosition;
 	private float zPosition;
 
 	private float distanceBetween;
-	private float time = 0;
-	public float frame;
+	public float timeBetween;
+	private float timeincrease;
+	public float speedUpTime;
 
-	public float maxDistance;
-	public float minDistance;
 
 	public int movespeed = 1;
 	public Vector3 userDirection = Vector3.left;
@@ -24,6 +28,7 @@ public class Create : MonoBehaviour {
 	// Use this for initializations
 	void Start () {
 		xPosition = generationPoint.transform.position.x;
+		 timeincrease = 0;
 
 
 	
@@ -31,34 +36,79 @@ public class Create : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-//		Debug.Log (generationPoint.transform.position);
-//		if (this.transform.position.x < generationPoint.transform.position.x) {
-//			Debug.Log (transform.position);
-//			distanceBetween = Random.Range (minDistance, maxDistance);
-//
-//			this.transform.position = new Vector3 (this.transform.position.x - objectWidth - distanceBetween, this.transform.position.y, this.transform.position.z);
-//
-//			Instantiate (obj, this.transform.position, this.transform.rotation);
-//
-//
-//		}
-
-		if (time == 0) {
-
-			zPosition = generationPoint.transform.position.z + Random.Range(0,5);
-
-			this.transform.position = new Vector3 (xPosition, this.transform.position.y, zPosition);
-
-			Instantiate (obj, this.transform.position, this.transform.rotation);
-
-			//transform.Translate(userDirection * movespeed * Time.deltaTime);
-
-			//time = Random.Range (100, 200);
-
-			time = frame;
+		if (Time.time == speedUpTime) {
+			timeincrease += 0.2F;
+			speedUpTime += Time.time;
 		}
 
-		time--;
+
+		if (Time.time > distanceBetween) {
+
+			int gameobj = Random.Range (0, 5);
+			if(gameobj == 0){
+				
+				distanceBetween = Time.time + timeBetween + timeincrease;
+
+				zPosition = generationPoint.transform.position.z + Random.Range (0, 5);
+
+				this.transform.position = new Vector3 (xPosition, this.transform.position.y, zPosition);
+
+				Instantiate (obj, this.transform.position, this.transform.rotation);
+			}
+			if(gameobj == 1){
+
+				distanceBetween = Time.time + timeBetween + timeincrease;
+
+				zPosition = generationPoint.transform.position.z + Random.Range (0, 5);
+
+				this.transform.position = new Vector3 (xPosition, this.transform.position.y, zPosition);
+
+				Instantiate (obj1, this.transform.position, this.transform.rotation);
+			}
+			if(gameobj == 2){
+
+				distanceBetween = Time.time + timeBetween + timeincrease;
+
+				zPosition = generationPoint.transform.position.z + Random.Range (0, 5);
+
+				this.transform.position = new Vector3 (xPosition, this.transform.position.y, zPosition);
+
+				Instantiate (obj2, this.transform.position, this.transform.rotation);
+			}
+			if(gameobj == 3){
+
+				distanceBetween = Time.time + timeBetween + timeincrease;
+
+				zPosition = generationPoint.transform.position.z + Random.Range (0, 5);
+
+				this.transform.position = new Vector3 (xPosition, this.transform.position.y, zPosition);
+
+				Instantiate (obj3, this.transform.position, this.transform.rotation);
+			}
+			if(gameobj == 4){
+
+				distanceBetween = Time.time + timeBetween + timeincrease;
+
+				zPosition = generationPoint.transform.position.z + Random.Range (0, 5);
+
+				this.transform.position = new Vector3 (xPosition, this.transform.position.y, zPosition);
+
+				Instantiate (obj4, this.transform.position, this.transform.rotation);
+			}
+			if(gameobj == 5){
+
+				distanceBetween = Time.time + timeBetween + timeincrease;
+
+				zPosition = generationPoint.transform.position.z + Random.Range (0, 5);
+
+				this.transform.position = new Vector3 (xPosition, this.transform.position.y, zPosition);
+
+				Instantiate (obj5, this.transform.position, this.transform.rotation);
+			}
+
+		}
+
+
 
 	}
 }
