77dc103738c3b50e6c3972148fd151aa6e4ee052
TomboyFluttershy <jen.oakley.artist@gmail.com>
2016-11-07 14:29:22 -0700

    New player movement scripts now set up within the project

--- Assets/Scripts/ReflectMovement.cs
+++ Assets/Scripts/ReflectMovement.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class ReflectMovement : MonoBehaviour 
 {
 	private int posTrack;
+	private int posTrackx;
 	// Use this for initialization
 	void Start ()
 	{
@@ -13,18 +14,32 @@ public class ReflectMovement : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Input.GetKeyDown(KeyCode.DownArrow) && (posTrack > 0))
+		if ((Input.GetKeyDown(KeyCode.DownArrow)&& (posTrack > 0))|| (Input.GetKeyDown(KeyCode.S)) && (posTrack > 0))
 		{
 			Vector3 position = this.transform.position;
 			position.y += 1.32f;
 			posTrack--;
 			this.transform.position = position;
 		}
-		if (Input.GetKeyDown(KeyCode.UpArrow) && (posTrack < 2))
+		if ((Input.GetKeyDown(KeyCode.UpArrow)&& (posTrack < 2))|| (Input.GetKeyDown(KeyCode.W)) && (posTrack < 2))
 		{
 			Vector3 position = this.transform.position;
 			position.y -= 1.32f;
 			posTrack++;
+			this.transform.position = position;
+		}
+		if ((Input.GetKeyDown(KeyCode.RightArrow)&& (posTrackx < 2))|| (Input.GetKeyDown(KeyCode.D)) && (posTrackx < 2))
+		{
+			Vector3 position = this.transform.position;
+			position.x += 1.32f;
+			posTrackx++;
+			this.transform.position = position;
+		}
+		if ((Input.GetKeyDown(KeyCode.LeftArrow)&& (posTrackx > 0))|| (Input.GetKeyDown(KeyCode.A)) && (posTrackx > 0))
+		{
+			Vector3 position = this.transform.position;
+			position.x -= 1.32f;
+			posTrackx--;
 			this.transform.position = position;
 		}
 	}
