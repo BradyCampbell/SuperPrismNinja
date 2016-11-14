dfe71c70e17545fc6b183753b051423d3b30b8e7
melody <changmelody2727@gmail.com>
2016-11-14 09:15:08 -0700

    object moving up and down

---
+++ Assets/Scripts/objMovingUpandDown.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class objMovingUpandDown : MonoBehaviour {
+	public int movespeed = 1;
+	public Vector3 upDirection = Vector3.up;
+	public Vector3 downDirection = Vector3.down;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float tim = Time.time;
+		if (tim % 2 < 1) {
+			transform.Translate (upDirection * movespeed * Time.deltaTime);
+		} else if(tim % 2 >= 1){
+			transform.Translate (downDirection * movespeed * Time.deltaTime);
+		}
+	}
+}
