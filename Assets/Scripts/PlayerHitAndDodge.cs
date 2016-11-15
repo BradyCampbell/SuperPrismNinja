2ae799621ec969c52a695820ae60dc73428a2937
TomboyFluttershy <jen.oakley.artist@gmail.com>
2016-11-14 19:35:17 -0700

    Powerups n' stuff from Brady!

--- Assets/Scripts/PlayerHitAndDodge.cs
+++ Assets/Scripts/PlayerHitAndDodge.cs
@@ -5,6 +5,7 @@ public class PlayerHitAndDodge : MonoBehaviour {
 
     public SpriteRenderer spriteRenderer;
 	private Animator animator;
+	public int health;
 	private int flickCount = 3;
 	private float flickTime = 0.1f;
     private bool dodging = false;
@@ -44,11 +45,11 @@ public class PlayerHitAndDodge : MonoBehaviour {
             {
                 Destroy(coll.gameObject);
 				SoundManagement.instance.sfxSource.Play ();
-				HealthAndPowManager.instance.hp--;
-				if (HealthAndPowManager.instance.hp > 0) {
+                health--;
+				if (health > 0) {
 					flicker ();
 				}
-				if (HealthAndPowManager.instance.hp <= 0)
+                if (health <= 0)
                 {
 					if (gameObject.tag == "PlayerShadow") {
 						deathAnim = GameObject.Find ("ninjaShadowDeathEffect");
